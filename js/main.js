f81ede88da7c1a3428fc897f4047ee883e30122b
solocyberengineer <solocyberengineer@gmail.com>
2023-11-23 12:06:28 +0200

    added js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+let minutesInput = document.getElementById('minutes');
+let convertBtn = document.querySelector('button');
+let output = document.getElementById('output');
 
-
-
-function minutesToSeconds(minutes){ // seconds / 60 = <minutes>  |----|  minutes * 60 = <seconds>
-    return minutes * 60
+function minutesToSeconds(){ // seconds / 60 = <minutes>  |----|  minutes * 60 = <seconds>
+    console.log(minutesInput.value)
+    output.textContent = `seconds: ${minutesInput.value * 60}`
 }
 function secondsToMinutes(seconds){
     return seconds / 60
 }
+
+minutesInput.oninput = minutesToSeconds
+
+// convertBtn.addEventListener('click', () => minutesToSeconds())
