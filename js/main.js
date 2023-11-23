f4e735979d3b49bbbb35f3e4a2a64cb11c2693d5
solocyberengineer <solocyberengineer@gmail.com>
2023-11-23 11:06:44 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+
+function minutesToSeconds(minutes){ // seconds / 60 = <minutes>  |----|  minutes * 60 = <seconds>
+    return minutes * 60
+}
+function secondsToMinutes(seconds){
+    return seconds / 60
+}
